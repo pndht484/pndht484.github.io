3e2578449eba81c1bc619a2ac4dfcbfaab591daf
pndht484 <60352973+pndht484@users.noreply.github.com>
2021-01-27 22:41:07 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,15 @@
 $(function() {
     $('#vegas').vegas({
         slides: [
-            { src: 'vegas_sample01.jpg' width="193" height="130"},
+            { src: 'vegas_sample01.jpg' },
             { src: 'vegas_sample02.jpg' },
             { src: 'vegas_sample03.jpg' },
             { src: 'vegas_sample04.jpg' },
             { src: 'vegas_sample05.jpg' }
         ],
         
-       
+       width:193,
+       height:130,
         transition: 'fade', //スライドを遷移させる際のアニメーション
         transitionDuration: 4000, //スライドの遷移アニメーションの時間
         delay: 2000, //スライド切り替え時の遅延時間
