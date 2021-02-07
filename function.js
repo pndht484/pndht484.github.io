854249a5eaa9fe51aa1105346637441072929973
pndht484 <60352973+pndht484@users.noreply.github.com>
2021-02-07 15:18:56 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,13 @@
 
 $(function() {
     $('#vegas').vegas({
+        overlay: true,
+        transition: 'flash', //スライドを遷移させる際のアニメーション
+        transitionDuration: 4000, //スライドの遷移アニメーションの時間
+        delay: 2000, //スライド切り替え時の遅延時間
+        animation: 'random', //スライド表示中のアニメーション
+        animationDuration: 4000, //スライド表示中のアニメーションの時間
+        
         slides: [
             { src: 'vegas_sample01.jpg' },
             { src: 'vegas_sample02.jpg' },
@@ -10,11 +17,6 @@ $(function() {
         ],
         
        
-        transition: 'fade', //スライドを遷移させる際のアニメーション
-        transitionDuration: 4000, //スライドの遷移アニメーションの時間
-        delay: 2000, //スライド切り替え時の遅延時間
-        animation: 'random', //スライド表示中のアニメーション
-        animationDuration: 4000, //スライド表示中のアニメーションの時間
        
     });
 });
